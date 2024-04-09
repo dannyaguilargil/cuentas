3bd52fce5c33e4c149f4c9d66478583c76fbb7ae
parrot <parrot@localhost.localdomain>
2024-04-09 17:23:06 -0500

    avanzando en gestion de informes

--- gestion_informes/urls.py
+++ gestion_informes/urls.py
@@ -6,6 +6,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     #### estas urls son de ejemplo de como quiero el frontend de informes
-    path('informe', views.informe, name='informe'), #vista django
+    path('informe/listado_informe', views.listado_informe, name='listado_informe'),
+    path('informe', views.informes, name='informe'), #vista django pasarla a vuejs
+    path('informe/listado', views.listado, name='listado'), #solo django
+    path('informe/ente-control', views.entecontrol, name='entecontrol'), #solo django
+    path('informe/dependencias', views.dependencias, name='dependencias'), #solo django
+  
   
 ] 
--- gestion_informes/views.py
+++ gestion_informes/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render, redirect
+from gestion_informes.models import informe
+from gestion_usuarios.models import usolicitudes
+from django.http.response import JsonResponse
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseNotAllowed
+from gestion_usuarios.models import usuario
 
 # Create your views here.
-def  informe(request):
+def  informes(request):
      return render(request, 'informes.html') 
+
+#ejemplo con solicitudes de usuarios provisionalmente
+def listado_informe(request):
+    usuarios = list(usolicitudes.objects.values())
+    data = {'informe': usuarios}
+    return JsonResponse(data)
+###listado de informe ###
+
+#ejemplo con solicitudes de usuarios provisionalmente
+def  listado(request):
+     usuarios = list(usolicitudes.objects.values())
+     datos = {'informe': usuarios}
+     return render(request, 'listado.html', {'datos': datos}) 
+
+def  entecontrol(request):
+     return render(request, 'entecontrol.html') 
+
+def  dependencias(request):
+     return render(request, 'dependencia.html') 
